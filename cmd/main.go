17cf46d8bbc0a577fae8c302497654ee6152f425
Iev Strygul <isl@dixa.com>
2020-10-12 09:02:11 +0200

    TDD-5 Adding test utils

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"github.com/strygul/ddt/internal/entity"
+	"net/http"
 )
 
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	// create response binary data
+	data := []byte(`{"foo": "bar"}`) // slice of bytes
+	// write `data` to response
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
 func main() {
-	fmt.Println("hello world")
-	emptyMap := make(map[string]string)
-	step := entity.Step{
-		Url:          "https://webhook.site/1b127957-0d09-4447-a754-2c3c56ca351e",
-		Method:       entity.Get,
-		Headers:      emptyMap,
-		Body:         "testing",
-		Placeholders: emptyMap,
-		Description:  "some description of what the step does",
-		JsonPath:     "",
-	}
-	step.ExecuteRequest()
+	//fmt.Println("hello world")
+	//emptyMap := make(map[string]string)
+	//step := entity.Step{
+	//	Url:          "https://webhook.site/1b127957-0d09-4447-a754-2c3c56ca351e",
+	//	Method:       entity.Get,
+	//	Headers:      emptyMap,
+	//	Body:         "testing",
+	//	Placeholders: emptyMap,
+	//	Description:  "some description of what the step does",
+	//	JsonPath:     "",
+	//}
+	//step.ExecuteRequest()
+
+	m := http.NewServeMux()
+	s := http.Server{Addr: ":8000", Handler: m}
+	m.HandleFunc("/", HelloServer)
+	s.ListenAndServe()
 
 	//
 	//request, err := step.ConstructRequest()
